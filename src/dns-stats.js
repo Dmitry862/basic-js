b8e620f0523c669c6e0a66e63ea1e0ee1f2e508c
Dmitry862 <rk3tt@mail.ru>
2025-01-02 17:01:56 +0300

    feat: add dns solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+  for (let i = 0; i < domains.length; i++) {
+    const parts = domains[i].split('.').reverse();
+    console.log(parts);
+    let currentDomain = '';
+    for (let j = 0; j < parts.length; j++) {
+      currentDomain = `${currentDomain}.${parts[j]}`;
+      console.log(currentDomain);
+      if (!result[currentDomain]) {
+        result[currentDomain] = 1;
+      } else {
+        result[currentDomain]++;
+      }
+    };
+  };
+  return result;
 }
 
 module.exports = {
